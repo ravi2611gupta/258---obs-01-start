22a8a694e47ee059d50bbb85b2cd610ce50e7a35
prashant <prashant@arc.ai>
2025-01-14 18:41:12 +0530

    262 videos done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -12,9 +12,36 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() {}
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe((count) => {
-      console.log(count);
+    // this.firstObsSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+
+    const customIntervalObservable = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'));
+        }
+        count++;
+      }, 1000);
     });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe(
+      (count: number) => {
+        console.log(count);
+      },
+      (error: Error) => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log('Completed');
+      }
+    );
   }
 
   ngOnDestroy(): void {
